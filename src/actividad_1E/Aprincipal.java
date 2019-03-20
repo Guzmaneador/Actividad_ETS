d8fd8bf0d6e461dc2046257cae6a780ea746bf59
munchi <munchi@munchi-PC>
2019-03-20 18:27:08 +0000

    primer commit

---
+++ src/actividad_1E/Aprincipal.java
@@ -0,0 +1,19 @@
+package actividad_1E;
+
+
+
+import actividad_1E_2.Tabla;
+import java.util.Scanner;
+/**
+ *
+ * @author munchi
+ */
+public class Aprincipal {
+    public static void main (String[] args){
+        Scanner Teclado = new Scanner(System.in);
+        System.out.print("Ingresa el tamaño del matriz n:");
+        Tabla.crearTabla(Teclado.nextInt());    
+    }
+    
+    
+}
