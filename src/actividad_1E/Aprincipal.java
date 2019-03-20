20af1aa6a2fc01d0d2ed1cc36fa3f8e7c1ef21bb
munchi <munchi@munchi-PC>
2019-03-20 18:31:44 +0000

    segundo commit

--- src/actividad_1E/Aprincipal.java
+++ src/actividad_1E/Aprincipal.java
@@ -12,7 +12,10 @@ public class Aprincipal {
     public static void main (String[] args){
         Scanner Teclado = new Scanner(System.in);
         System.out.print("Ingresa el tamaño del matriz n:");
-        Tabla.crearTabla(Teclado.nextInt());    
+        Tabla.crearTabla(Teclado.nextInt());
+        //vamos hacer un print 
+        System.out.println("HOLA HOLA CARACOLA");
+        //fin de la preuba
     }
     
     
